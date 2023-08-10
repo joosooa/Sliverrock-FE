ada16a521aec999505bf1833a2214acdcd04da76
Su-daa <subin.kk019@gmail.com>
2023-08-10 10:34:31 +0900

    Feat: tab 구현

---
+++ src/components/Tab.js
@@ -0,0 +1,34 @@
+import Nav from "react-bootstrap/Nav";
+import "bootstrap/dist/css/bootstrap.css";
+import "../styles/Tab.css";
+
+function Tab() {
+  return (
+    <>
+      <Nav justify defaultActiveKey="/home" className="fixed-bottom">
+        <Nav.Item>
+          <Nav.Link className="navlink" href="/home">
+            추천친구
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link className="navlink" href="/">
+            동호회
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link className="navlink" href="/">
+            매칭
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link className="navlink" href="/MyPage">
+            내정보
+          </Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </>
+  );
+}
+
+export default Tab;
