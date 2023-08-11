0fe6dba5a82db6ee3b9cc3d88cb86a0a0b07e822
Su-daa <subin.kk019@gmail.com>
2023-08-11 12:23:24 +0900

    init: 후에 redux사용하도록 store.js 파일 생성

---
+++ src/app/store.js
@@ -0,0 +1,9 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// let userInfo = createSlice({
+//   name: "userInfo",
+//   initialState,
+//   reducers: {
+//     setUserInfo: (state, action) => {},
+//   },
+// });
