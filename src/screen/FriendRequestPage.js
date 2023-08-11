898f12c52d114022e912f23702963de26e4be3e2
Su-daa <subin.kk019@gmail.com>
2023-08-11 12:22:16 +0900

    Feat: 매칭 탭 클릭시 친구 매칭 탭 띄우기

--- src/screen/FriendRequestPage.js
+++ src/screen/FriendRequestPage.js
@@ -2,17 +2,20 @@ import React from "react";
 import Profile from "../components/Profile";
 import "../styles/HomePage.css";
 import Tab from "../components/Tab";
+import MatchingTab from "../components/MatchingTab";
 
 function FriendRequestPage() {
   return (
     <>
-      <h1 className="title">추천 친구</h1>
+      <h1 className="title">N명의 실버락이 있어요</h1>
       <div className="profile-box">
         <Profile />
       </div>
       <div className="btn-container">
-        <button className="custom-btn btn-11">친구신청</button>
+        <button className="custom-btn btn-11">수락</button>
+        <button className="custom-btn btn-11">거절</button>
       </div>
+      <MatchingTab />
       <Tab />
     </>
   );
