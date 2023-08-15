5d244de06c13dce1cf1115e9b788541254ae438b
Su-daa <subin.kk019@gmail.com>
2023-08-15 21:16:43 +0900

    Feat: 프로필 정보 띄우기

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,18 +1,21 @@
 import "../styles/Profile.css";
 import profileImg from "../styles/profile.jpg";
 
-function Profile() {
+function Profile(user) {
+  const imgUrl = user.getS3Res.imgUrl;
   return (
     <div className="box-container">
       <div className="profile-box">
         <div className="profile-img-box">
-          <img className="profile-img" src={profileImg} alt="profile-img"></img>
+          <img className="profile-img" src={imgUrl} alt="profile-img"></img>
         </div>
         <div className="profile-info">
-          <h4 className="profile-info-text">이름 </h4>
-          <h4 className="profile-info-text">나이 년생</h4>
-          <h4 className="profile-info-text">지역 </h4>
-          <h4>소개 </h4>
+          <h4 className="profile-info-text">
+            이름 {user.nickname} ({user.gender})
+          </h4>
+          <h4 className="profile-info-text">나이 {user.birth}년생</h4>
+          <h4 className="profile-info-text">지역 {user.region}</h4>
+          <h4>소개 {user.introduce}</h4>
         </div>
       </div>
     </div>
