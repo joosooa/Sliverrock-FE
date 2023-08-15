a2a4141c61d0b86edfcecf87ccbdf7b00b180b19
Su-daa <subin.kk019@gmail.com>
2023-08-15 21:15:07 +0900

    Feat: 친구 목록 페이지

--- src/components/FriendList.js
+++ src/components/FriendList.js
@@ -2,23 +2,38 @@ import React from "react";
 import "../styles/friendList.css";
 import friendListImg from "../styles/friendList.jpg";
 import { BsTelephoneFill } from "react-icons/bs";
+import Carousel from "react-material-ui-carousel";
 
-const Club = () => {
+function FriendList(props) {
+  const handleCallButtonClick = () => {
+    alert("전화걸기");
+  };
+
+  let imgUrl = props.getS3Res.imgUrl;
+
   return (
-    <div className="image-container">
-      <img src={friendListImg} alt="friendListImg" className="friendListImg" />
-      <div>
-        <h3 className="container">
-          <h2 className="container">순자</h2>70년생 여자
-        </h3>
-        <button className="callButton">
-          <div className="iconImg">
-            <BsTelephoneFill />
+    <div>
+      <Carousel>
+        {props.map((friend, index) => (
+          <div key={index} className="image-container">
+            <img src={imgUrl} alt="friendListImg" className="friendListImg" />
+            <div>
+              <h3 className="container">
+                <h2 className="container">{friend.nickname}</h2>
+                {friend.birth}&nbsp;
+                {friend.gender}
+              </h3>
+              <button className="callButton" onClick={handleCallButtonClick}>
+                <div className="iconImg">
+                  <BsTelephoneFill />
+                </div>
+              </button>
+            </div>
           </div>
-        </button>
-      </div>
+        ))}
+      </Carousel>
     </div>
   );
-};
+}
 
-export default Club;
+export default FriendList;
